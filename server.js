6d37c86117cedf2b9198854557f4ceac5fc384c7
Lunirs <dhong0925@gmail.com>
2022-06-13 19:03:25 -0400

    Added dotenv and created connection to database

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
+require("dotenv").config();
+
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const consoleTable = require("console.table");
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  },
+  console.log("Database successfully connected")
+);
+
+const init = () => questions();
+
+init();
