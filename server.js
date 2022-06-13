014d7b55c00afbe4e8e4113664df757cec5d6486
Lunirs <dhong0925@gmail.com>
2022-06-13 19:28:05 -0400

    Added initial inquirer prompt and beginning of switch case

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 
+//Dependencies
+
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const consoleTable = require("console.table");
+
+// Creating a connection with my database
 
 const db = mysql.createConnection(
   {
@@ -14,6 +18,45 @@ const db = mysql.createConnection(
   console.log("Database successfully connected")
 );
 
+// initialization function that will run at load and start up initial inquirer
 const init = () => questions();
+
+// initial inquirer  prompting them on the things they can do with app.
+const questions = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Please select what you would like to do.",
+        choices: [
+          "View all employees",
+          "View all roles",
+          "View all departments",
+          "Add an employee",
+          "Add a new role",
+          "Add a new department",
+          "Update employee role",
+        ],
+        name: "userChoice",
+      },
+    ])
+    .then((response) => {
+      // switch case depending on the choice they made
+      switch (response.userChoice) {
+        // add cases and for each case have it run a appropriate function that runs
+        // probably will need to add more depending on if i can do bonus
+        case "View all employees":
+          // Need function to see all employees
+          viewAllEmployees();
+          break;
+
+        // Need function to see all roles
+        // Need function to see all departments
+        // Need function to add new role
+        // Need function to add new department
+        // Need function to update employee role
+      }
+    });
+};
 
 init();
